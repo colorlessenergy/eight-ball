aa987cc0baf7e660ff12f90b7ded1808872da634
colorlessenergy <brianmunozdev@gmail.com>
2017-02-02 05:57:16 -0800

    added interactions, custom question, and more responses

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-var question  = document.getElementById("question")
-var button = document.getElementById("questionData")
-var responses = ["It is decidedly so", "no", "Very doubtful"];
-
+var question  = document.getElementById("question");
+var button = document.getElementById("questionData");
+var showQuestion = document.getElementById("showQuestion");
+var showAnswer = document.getElementById("answer");
+var responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it",
+                 "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again",
+                 "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+                 "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
 button.addEventListener("click", function () {
   if (question.value !== "") {
-    alert(responses[Math.floor(Math.random() * 2)]);
+    switch (question.value) {
+      case "who are you":
+      showQuestion.innerHTML = question.value;
+      showAnswer.innerHTML = "the magical 8-Ball"
+        break;
+      default:
+      showQuestion.innerHTML = question.value;
+      showAnswer.innerHTML = responses[Math.floor(Math.random() * responses.length-1)];
+    }
   } else {
     alert("insert something into the text box");
   }
