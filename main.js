e130622166b5edd824b6bfa7ea59bf1798fadc19
colorlessenergy <brianmunozdev@gmail.com>
2017-02-10 18:56:41 -0800

    added punctuation and enter works when pressing submit

--- main.js
+++ main.js
@@ -6,19 +6,27 @@ var responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes,
                  "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again",
                  "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
                  "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
-button.addEventListener("click", function () {
-  if (question.value !== "") {
-    switch (question.value) {
-      case "who are you":
-      showQuestion.innerHTML = question.value;
-      showAnswer.innerHTML = "the magical 8-Ball"
+button.addEventListener("click", function (e) {
+    if (question.value !== "") {
+      switch (question.value.toLowerCase()) {
+        case "who are you":
+        showQuestion.innerHTML = "Question: " + question.value;
+        showAnswer.innerHTML = "Answer: " +  "the magical 8-Ball";
         break;
-      default:
-      showQuestion.innerHTML = "Question: " + question.value;
-      showAnswer.innerHTML = "Answer: " + responses[Math.floor(Math.random() * responses.length-1)];
+        default:
+        showQuestion.innerHTML = "Question: " + question.value;
+        showAnswer.innerHTML = "Answer: " + responses[Math.floor(Math.random() * responses.length-1)];
+      }
+    } else {
+      showQuestion.innerHTML = "insert something into the textbox"
+      showAnswer.innerHTML = "insert something into the textbox"
     }
-  } else {
-    showQuestion.innerHTML = "insert something into the textbox"
-    showAnswer.innerHTML = "insert something into the textbox"
-  }
+    question.value = "";
+});
+
+question.addEventListener("keyup", function (event) {
+  event.preventDefault();
+  if (event.keyCode == 13) {
+    button.click();
+}
 });
